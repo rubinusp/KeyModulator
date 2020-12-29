b11649b7578ae14cfab7cdee36c99a1c4e1e055a
Gordon Poon <rubinusp@gmail.com>
2020-12-28 22:23:20 -0700

    handle write exception / add verbose enability to the program

--- KeyModulator.py
+++ KeyModulator.py
@@ -34,13 +34,13 @@ class KeyModulator:
         self.freq_ratio = 2 ** (1/12)
 
     def shift(self, semitone):
-        print("Shifting semitones")
+        if self.verbose:
+            print("Shifting semitones")
+
         # Semitone = 1, i.e. one semitone up
         # Semitone = 0, i.e. no shifting
         # Semitone = -1, i.e. one semitone down
         shift = self.freq_ratio ** semitone
-        print(f"Shift = {shift}")
-        print()
 
         frames = self._create_frames()
         frames = self._apply_window(frames)
@@ -49,23 +49,28 @@ class KeyModulator:
         self.new_audio = self._resample()
 
     def _resample(self):
-        print("Resampling the synthesized audio to change the key")
-        print()
+        if self.verbose:
+            print("Resampling the synthesized audio to change the key")
+            print()
+
         sam_x = np.linspace(0, self.new_n_samples, num=self.n_samples, endpoint=False)
         sam_y = np.interp(sam_x, [i for i in range(self.new_n_samples)], self.new_audio)
         return sam_y
 
     def _synthesize(self, frames, slow):
-        print("Synthesizing frames")
+        if self.verbose:
+            print("Synthesizing frames")
+
         self.new_n_samples = int(slow * self.n_samples)
         audio = np.zeros(self.new_n_samples)
         new_hop_size = int((self.new_n_samples - len(frames[-1])) / (self.n_frames - 1))
         new_duration = self.new_n_samples / self.sample_rate
 
-        print(f"New number of samples = {self.new_n_samples}")
-        print(f"New duration = {new_duration} {str(dt.timedelta(seconds=new_duration))}")
-        print(f"New hop size = {new_hop_size}")
-        print()
+        if self.verbose:
+            print(f"New number of samples = {self.new_n_samples}")
+            print(f"New duration = {new_duration} {str(dt.timedelta(seconds=new_duration))}")
+            print(f"New hop size = {new_hop_size}")
+            print()
 
         i = 0
         for frame in frames:
@@ -75,15 +80,18 @@ class KeyModulator:
         return audio
 
     def _apply_window(self, frames):
-        print("Applying Hanning window")
-        print()
+        if self.verbose:
+            print("Applying Hanning window")
+            print()
+
         windowed_frames = [frame * np.hanning(len(frame)) for frame in frames]
         return windowed_frames
 
     def _create_frames(self):
-        print("Creating frames")
-        audio = self.audio[:, 0].ravel()
+        if self.verbose:
+            print("Creating frames")
 
+        audio = self.audio[:, 0].ravel()
         frames = []
         i = 0
         while True:
@@ -97,11 +105,12 @@ class KeyModulator:
 
         self.n_frames = len(frames)
         # frame_len / 4 * (n_frames - 1) + last_frame_len = n_samples
-        print(f"Frame length = {self.frame_len}")
-        print(f"Length of last frame = {len(frames[-1])}")
-        print(f"Hop size = {self.hop_size}")
-        print(f"Number of frames = {self.n_frames}")
-        print()
+        if self.verbose:
+            print(f"Frame length = {self.frame_len}")
+            print(f"Length of last frame = {len(frames[-1])}")
+            print(f"Hop size = {self.hop_size}")
+            print(f"Number of frames = {self.n_frames}")
+            print()
 
         # Checking the frames overlap
         # print(frames[0][12000:12000+4])
@@ -112,26 +121,35 @@ class KeyModulator:
     def read(self, path):
 
         try:
-            print(f"Reading audio from {path}")
+            if self.verbose:
+                print(f"Reading audio from {path}")
+
             self.sample_rate, self.audio = sp_io_wav.read(path)
             self.n_samples = len(self.audio)
             self.n_channels = self.audio.shape[1]
             self.duration = self.n_samples / self.sample_rate
 
-            print(f"Audio matrix = {self.audio}")
-            print(f"Sample rate = {self.sample_rate}")
-            print(f"Number of samples = {self.n_samples}")
-            print(f"Number of channels = {self.n_channels}")
-            print(f"Duration = {self.duration} {str(dt.timedelta(seconds=self.duration))}")
-            print()
+            if self.verbose:
+                print(f"Audio matrix = {self.audio}")
+                print(f"Sample rate = {self.sample_rate}")
+                print(f"Number of samples = {self.n_samples}")
+                print(f"Number of channels = {self.n_channels}")
+                print(f"Duration = {self.duration} {str(dt.timedelta(seconds=self.duration))}")
+                print()
         except Exception as e:
             print(e)
             exit(-2)
 
     def write(self, path):
-        print(f"Writing audio to {path}")
-        normalized_audio = np.int16((self.new_audio / self.new_audio.max()) * 32767)
-        sp_io_wav.write(path, self.sample_rate, normalized_audio)
+        if self.verbose:
+            print(f"Writing audio to {path}")
+
+        try:
+            normalized_audio = np.int16((self.new_audio / self.new_audio.max()) * 32767)
+            sp_io_wav.write(path, self.sample_rate, normalized_audio)
+        except Exception as e:
+            print(e)
+            exit(-2)
 
     def plot(self):
         plot_rate = 100     # Number of plotted points per second
@@ -153,6 +171,12 @@ def parse_argv(argv):
 
     try:
         opts, args = getopt.getopt(argv, "hv")
+
+        verbose = False
+        for opt, val in opts:
+            if opt == "-v":
+                verbose = True
+
         if len(args) != 3:
             print(f"usage: {USAGE}")
             exit(-2)
@@ -160,7 +184,7 @@ def parse_argv(argv):
         input = args[0]
         output = args[1]
         shift = args[2]
-        return False, False, input, output, int(shift)
+        return False, verbose, input, output, int(shift)
     except getopt.GetoptError:
         print(f"usage: {USAGE}")
         exit(-2)
